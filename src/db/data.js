2b2a65e9994044922de76d89714254dec9d69824
zimbora <lucas.ua.eet@gmail.com>
2025-07-12 15:45:48 +0100

    src/db/data: handles json data - Adds updateJson call - Adds addJsonLog

--- src/db/data.js
+++ src/db/data.js
@@ -5,24 +5,24 @@ var self = module.exports = {
 
 	checkEntry : async (table,deviceId)=>{
 
-	  return new Promise((resolve,reject) => {
+		return new Promise((resolve,reject) => {
 
-	    let query = "SELECT * FROM ?? where device_id = ?";
-	    let args = [table,deviceId];
-	    query = mysql.format(query,args);
+		let query = "SELECT * FROM ?? where device_id = ?";
+		let args = [table,deviceId];
+		query = mysql.format(query,args);
 
-	    $.db.queryRow(query)
-	    .then( rows => {
-	      if(rows.length > 0)
-	        return resolve(rows[0]);
-	      else
-	        return resolve(null);
-	    })
-	    .catch( err => {
-	      console.log(err);
-	      return resolve(null);
-	    });
-	  });
+		$.db.queryRow(query)
+		.then( rows => {
+			if(rows.length > 0)
+				return resolve(rows[0]);
+			else
+				return resolve(null);
+		})
+		.catch( err => {
+			console.log(err);
+			return resolve(null);
+			});
+		});
 	},
 
 	update : async (table, deviceId, topic, payload)=>{
@@ -54,41 +54,41 @@ var self = module.exports = {
 				topic = topic.substring(index+1);
 
 				if(db_columns.hasOwnProperty(key)){ // check if db has a column for this key
-				  // create JSON struct with the remaining topic
-				  // table is the table
-				  // key is the column
+					// create JSON struct with the remaining topic
+					// table is the table
+					// key is the column
 
-				  // build object with the remaining topic and respective data
-				  if(typeof data === "object")
-				    obj[key] = JSON.stringify(parser.pathIntoObject(topic,JSON.stringify(data)))
-				  else
-				    obj[key] = JSON.stringify(parser.pathIntoObject(topic,data));
+					// build object with the remaining topic and respective data
+					if(typeof data === "object")
+						obj[key] = JSON.stringify(parser.pathIntoObject(topic,JSON.stringify(data)))
+					else
+						obj[key] = JSON.stringify(parser.pathIntoObject(topic,data));
 
-				  let exists = await self.checkEntry(table,deviceId);
-				  if(!exists){
-				    obj['device_id'] = deviceId;
-				    obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
-				    $.db.insert(table,obj)
-				    .then((rows)=>{
-				    	return resolve(rows);
-				    })
-				    .catch((err)=>{
-				    	console.log(err);
-				    	return resolve();
-				    })
-				  }else{
-				    let filter = {
-				      device_id : deviceId
-				    }
-				    $.db.update(table,obj,filter)
-				    .then((rows)=>{
-				    	return resolve(rows);
-				    })
-				    .catch((err)=>{
-				    	console.log(err);
-				    	return resolve();
-				    })
-				  }
+					let exists = await self.checkEntry(table,deviceId);
+					if(!exists){
+						obj['device_id'] = deviceId;
+						obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
+						$.db.insert(table,obj)
+						.then((rows)=>{
+							return resolve(rows);
+						})
+						.catch((err)=>{
+							console.log(err);
+							return resolve();
+						})
+					}else{
+						let filter = {
+							device_id : deviceId
+						}
+						$.db.update(table,obj,filter)
+						.then((rows)=>{
+							return resolve(rows);
+						})
+						.catch((err)=>{
+							console.log(err);
+							return resolve();
+						})
+					}
 
 				}else return resolve();
 			}else{
@@ -115,7 +115,7 @@ var self = module.exports = {
 						})
 					}else{
 						let filter = {
-						  device_id : deviceId
+							device_id : deviceId
 						}
 
 						$.db.update(table,obj,filter)
@@ -132,67 +132,184 @@ var self = module.exports = {
 		})
 	},
 
+	updateJson : async (table, deviceId, payload)=>{
+
+		return new Promise( async (resolve, reject) => {
+		
+			if (!payload || typeof payload !== 'object') 
+				return resolve();
+
+			let obj = {
+				updatedAt: moment().utc().format('YYYY-MM-DD HH:mm:ss'),
+				device_id: deviceId,
+			};
+
+			// Get columns info
+			const db_columns = $.models.get(table);
+			if (db_columns == null) 
+				return resolve();
+
+			// Prepare data for insertion
+			for (let key in payload) {
+				if (db_columns.hasOwnProperty(key)) {
+					let value = payload[key];
+
+					// Convert object to JSON string if needed
+					if (typeof value === 'object') {
+						value = JSON.stringify(value);
+					}
+
+					obj[key] = value;
+				}
+			}
+
+			let exists = await self.checkEntry(table,deviceId);
+			if(!exists){
+				obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
+				$.db.insert(table,obj)
+				.then((rows)=>{
+					return resolve(rows);
+				})
+				.catch((err)=>{
+					console.log(err);
+					return resolve();
+				})
+			}else{
+				let filter = {
+					device_id : deviceId
+				}
+				$.db.update(table,obj,filter)
+				.then((rows)=>{
+					return resolve(rows);
+				})
+				.catch((err)=>{
+					console.log(err);
+					return resolve();
+				})
+			}
+
+			return resolve();
+		});
+	},
+
 	addLog : async (table, deviceId, topic, payload)=>{
 
 	  return new Promise((resolve,reject) => {
 
 	    if(!payload) return resolve();
 
-		let data = null;
-		// check if data is in JSON format
-		try{
-			data = JSON.parse(payload);
-		}catch(e){
-			data = payload;
-		}
-
-		let obj = {
-			updatedAt : moment().utc().format('YYYY-MM-DD HH:mm:ss')
-		}
-
-		let db_columns = $.models.get(table);
-		if(db_columns == null)
-			return resolve();
-
-		let index = topic.indexOf("/");
-
-		if(index > -1){ // check if topic has subtopics
-			let key = topic.substring(0,index);
-			topic = topic.substring(index+1);
-
-			if(db_columns.hasOwnProperty(key)){ // check if db has a column for this key
-			  // create JSON struct with the remaining topic
-			  // key is the column
-
-			  // build object with the remaining topic and respective data
-			  if(typeof data === "object")
-			    obj[key] = JSON.stringify(parser.pathIntoObject(topic,JSON.stringify(data)))
-			  else
-			    obj[key] = JSON.stringify(parser.pathIntoObject(topic,data));
-
-			  obj['device_id'] = deviceId;
-			  obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
-			  $.db.insert(table,obj)
-
+			let data = null;
+			// check if data is in JSON format
+			try{
+				data = JSON.parse(payload);
+			}catch(e){
+				data = payload;
 			}
-		}else{
 
-			let key = topic;
-			if(db_columns.hasOwnProperty(key)){ // check if db has a column for this key
-			  // build object with respective data
-			  if(typeof data === "object")
-			    obj[key] = JSON.stringify(data);
-			  else
-			    obj[key] = data;
-
-			  obj['device_id'] = deviceId;
-			  obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
-			  $.db.insert(table,obj);
+			let obj = {
+				updatedAt : moment().utc().format('YYYY-MM-DD HH:mm:ss')
 			}
-		}
 
-	    return resolve();
+			let db_columns = $.models.get(table);
+			if(db_columns == null)
+				return resolve();
+
+			let index = topic.indexOf("/");
+
+			if(index > -1){ // check if topic has subtopics
+				let key = topic.substring(0,index);
+				topic = topic.substring(index+1);
+
+				if(db_columns.hasOwnProperty(key)){ // check if db has a column for this key
+					// create JSON struct with the remaining topic
+					// key is the column
+
+					// build object with the remaining topic and respective data
+					if(typeof data === "object")
+						obj[key] = JSON.stringify(parser.pathIntoObject(topic,JSON.stringify(data)))
+					else
+						obj[key] = JSON.stringify(parser.pathIntoObject(topic,data));
+
+					obj['device_id'] = deviceId;
+					obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
+					$.db.insert(table,obj)
+					.then((rows)=>{
+						return resolve(rows);
+					})
+					.catch((err)=>{
+						console.log(err);
+						return resolve();
+					})
+
+				}else
+					return resolve();
+			}else{
+
+				let key = topic;
+				if(db_columns.hasOwnProperty(key)){ // check if db has a column for this key
+					// build object with respective data
+					if(typeof data === "object")
+						obj[key] = JSON.stringify(data);
+					else
+						obj[key] = data;
+
+					obj['device_id'] = deviceId;
+					obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
+					$.db.insert(table,obj)
+					.then((rows)=>{
+						return resolve(rows);
+					})
+					.catch((err)=>{
+						console.log(err);
+						return resolve();
+					})
+				}else
+					return resolve();
+			}
+
 	  });
+	},
+
+	addJsonLog: async (table, deviceId, dataObject) => {
+		return new Promise((resolve, reject) => {
+			if (!dataObject || typeof dataObject !== 'object') 
+				return resolve();
+
+			let obj = {
+				updatedAt: moment().utc().format('YYYY-MM-DD HH:mm:ss'),
+				device_id: deviceId,
+				createdAt: moment().utc().format('YYYY-MM-DD HH:mm:ss')
+			};
+
+			// Get columns info
+			const db_columns = $.models.get(table);
+			if (db_columns == null) 
+				return resolve();
+
+			// Prepare data for insertion
+			for (let key in dataObject) {
+				if (db_columns.hasOwnProperty(key)) {
+					let value = dataObject[key];
+
+					// Convert object to JSON string if needed
+					if (typeof value === 'object') {
+						value = JSON.stringify(value);
+					}
+
+					obj[key] = value;
+				}
+			}
+
+			//console.log("updateJSONlog")
+			$.db.insert(table, obj)
+			.then((rows)=>{
+				return resolve(rows);
+			})
+			.catch((err)=>{
+				console.log(err);
+				return resolve();
+			})
+		});
 	},
 
 	deleteLogs : async (table, deviceId, topic, payload)=>{
@@ -223,7 +340,13 @@ var self = module.exports = {
 			  obj['device_id'] = deviceId;
 			  obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
 			  $.db.insert(table,obj)
-
+			  .then((rows)=>{
+				return resolve(rows);
+			  })
+			.catch((err)=>{
+				console.log(err);
+				return resolve();
+			})
 			}
 		}else{
 
@@ -237,7 +360,7 @@ var self = module.exports = {
 
 			  obj['device_id'] = deviceId;
 			  obj['createdAt'] = moment().utc().format('YYYY-MM-DD HH:mm:ss');
-			  $.db.insert(table,obj);
+			  $.db.insert(table,obj)
 			}
 		}
 
